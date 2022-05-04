19067a132f19de38461ca5fef9b3db7def47e9c1
marcoripa96 <marco.ripa.96@gmail.com>
2022-05-04 20:58:48 +0200

    Styled wrong ts typing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { styled } from '@nextui-org/react'
+
+const Comp = styled('div', {
+  display: 'flex'
+})
 
 const Home: NextPage = () => {
   return (
